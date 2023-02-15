dc37985df2bd59e1052c1ed37a1878f2dd0635a2
AlexanderTarler <saltdev@dev-salts-MBP>
2023-02-15 15:16:37 +0100

    Mobs Routes completed

--- routes/mobs.js
+++ routes/mobs.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var { v4: uuidv4 } = require('uuid');
+
+let mobs = [];
 
 /* GET mobs listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  res.send(mobs);
+});
+
+router.get('/:id', function (req, res) {
+  const mob = mobs.find((mob) => mob.mobId == req.params.id);
+  res.send(mob);
+});
+
+router.post('/', function (req, res, next) {
+  const newMob = {
+    name: req.query.name,
+    mobId: uuidv4(),
+  };
+  mobs.push(newMob);
+  res.send(newMob);
+});
+
+router.delete('/:id', function (req, res) {
+  const mob = mobs.find((mob) => mob.mobId == req.params.id);
+  mobs.pop(mob);
+  res.send(mob);
 });
 
 module.exports = router;
