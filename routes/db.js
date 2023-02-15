d470bfdabf57593de9b5a3c95113b142f8d2a2d5
AlexanderTarler <saltdev@dev-salts-MBP>
2023-02-15 17:12:56 +0100

    add db test for testing

--- routes/db.js
+++ routes/db.js
@@ -1,6 +1,11 @@
-let mobs = [];
+let mobs = [{ name: 'Plan-B', mobId: '1' }];
 
-let members = [];
+let members = [
+  { memberId: '3', name: 'Negar', mobId: '1' },
+  { memberId: '3', name: 'Negar', mobId: '1' },
+  { memberId: '3', name: 'Negar', mobId: '1' },
+  { memberId: '3', name: 'Negar', mobId: '1' },
+];
 
 module.exports.mobs = mobs;
 module.exports.members = members;
