08d0eb8b4e720f48abeebc0b6d6b3daa134b8045
AlexanderTarler <saltdev@dev-salts-MBP>
2023-02-17 09:18:54 +0100

    Changed the mob db

--- routes/db.js
+++ routes/db.js
@@ -1,10 +1,10 @@
 let mobs = [{ name: 'Plan-B', mobId: '1' }];
 
 let members = [
-  { memberId: '3', name: 'Negar', mobId: '1' },
-  { memberId: '3', name: 'Negar', mobId: '1' },
-  { memberId: '3', name: 'Negar', mobId: '1' },
-  { memberId: '3', name: 'Negar', mobId: '1' },
+  { memberId: '1', name: 'Pani', mobId: '1' },
+  { memberId: '2', name: 'Luca', mobId: '1' },
+  { memberId: '3', name: 'Alex', mobId: '1' },
+  { memberId: '4', name: 'Negar', mobId: '1' },
 ];
 
 module.exports.mobs = mobs;
